518d801425bb701c84489ffc6d39f4c4e4e2e69c
aishabhamla <80135062+aishabhamla@users.noreply.github.com>
2022-04-24 17:24:17 -0400

    Delete hashtag.py

--- hashtag.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-import pandas as pd
-
-train = pd.read_csv("/kaggle/input/crowdflower-weather-twitter/train.csv")
-test = pd.read_csv('/kaggle/input/crowdflower-weather-twitter/test.csv')
-print(train.shape)
